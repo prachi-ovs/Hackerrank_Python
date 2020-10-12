fd9fe156481e7edc2e4e869ffd09b87362632422
Prachi <prachi.ovs@gmail.com>
2020-10-12 13:51:20 -0400

    Changes to for loop in stringValidators.py code file

--- stringValidators/stringValidators.py
+++ stringValidators/stringValidators.py
@@ -5,12 +5,16 @@ def string_validator(s):
     flag_lower = 0
     flag_upper = 0
     for i in s:
-        flag_alnum = 1 if i.isalnum() else 0
-        flag_alpha = 1 if i.isalpha() else 0
-        flag_num = 1 if i.isdigit() else 0
-        flag_lower = 1 if i.islower() else 0
-        flag_upper = 1 if i.isupper() else 0
-
+        if i.isalnum():
+            flag_alnum = 1
+        if i.isalpha():
+            flag_alpha = 1
+        if i.isdigit():
+            flag_num = 1
+        if i.islower():
+            flag_lower = 1
+        if i.isupper():
+            flag_upper = 1
     print("True" if flag_alnum == 1 else "False")
     print("True" if flag_alpha == 1 else "False")
     print("True" if flag_num == 1 else "False")
