b9927daad93351e8c461e460906047a0206e2a0b
Prachi <prachi.ovs@gmail.com>
2020-10-12 10:31:30 -0400

    Changes to code file

--- swapCase/swapCase.py
+++ swapCase/swapCase.py
@@ -1,24 +1,24 @@
 def swap_case(s):
-    newstring =''
+    newstring = ''
     for a in s:
-# Checking for lowercase letter and converting to uppercase.
-        if (a.isupper()) == True:
-            newstring+=(a.lower())
-# Checking for uppercase letter and converting to lowercase.
-        elif (a.islower()) == True:
-            newstring+=(a.upper())
-# Checking for whitespace letter and adding it to the new string
-        elif (a.isspace()) == True:
-            newstring+= a
-# Checking for numeric and adding it to the new string
-        elif (a.isnumeric()) == True:
-            newstring+= a
-# Checking for non alphanumeric characters and adding it to the new string
-        elif (a.isalnum()) == False:
-            newstring+= a
-
+        # Checking for lowercase letter and converting to uppercase.
+        if a.isupper():
+            newstring += (a.lower())
+        # Checking for uppercase letter and converting to lowercase.
+        elif a.islower():
+            newstring += (a.upper())
+        # Checking for whitespace letter and adding it to the new string
+        elif a.isspace():
+            newstring += a
+        # Checking for numeric and adding it to the new string
+        elif a.isnumeric():
+            newstring += a
+        # Checking for non alphanumeric characters and adding it to the new string
+        elif not (a.isalnum()):
+            newstring += a
 
     return newstring
+
 
 if __name__ == '__main__':
     s = input()
