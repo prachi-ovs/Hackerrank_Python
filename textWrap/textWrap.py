9540d08cb31a5d21c2eb7c27361c5db578427822
Prachi <prachi.ovs@gmail.com>
2020-10-26 08:03:36 -0400

    Add the code file and the problem statement

---
+++ textWrap/textWrap.py
@@ -0,0 +1,16 @@
+import textwrap
+
+
+def wrap(s, width):
+    # for i in range(len(s)):
+    wordSlice = []
+    while s:
+        wordSlice.append(s[0:width])
+        s = s[width:]
+    return '\n'.join(wordSlice)
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
